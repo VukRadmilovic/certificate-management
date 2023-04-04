aa94cb6d3e3b9ea2c7e18ddd1b9764e090845c58
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2023-04-04 21:16:32 +0200

    Add max length to denial reason

--- app/src/main/java/ftn/app/dto/CertificateRequestDenialDTO.java
+++ app/src/main/java/ftn/app/dto/CertificateRequestDenialDTO.java
@@ -4,6 +4,7 @@ package ftn.app.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotEmpty;
 
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotEmpty;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CertificateRequestDenialDTO {
+    @Length(max = 255, message = "{maxLength}")
     @NotEmpty
     private String denialReason;
 }
