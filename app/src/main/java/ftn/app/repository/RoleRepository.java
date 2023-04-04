b948e3eafb8734f88240f769178a9781953840b2
Marko Milijanovic <markomilijanovic01@gmail.com>
2023-04-04 22:14:44 +0200

    Bug fixes

--- app/src/main/java/ftn/app/repository/RoleRepository.java
+++ app/src/main/java/ftn/app/repository/RoleRepository.java
@@ -4,5 +4,8 @@ import ftn.app.model.Role;
 import ftn.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+    public Optional<Role> findByName(String name);
 }
